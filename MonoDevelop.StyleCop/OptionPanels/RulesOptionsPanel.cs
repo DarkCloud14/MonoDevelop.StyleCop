9fd480eb9ea58e88a215256cdf269e3b66f973f6
Alexander Jochum <darkcloud14@gmail.com>
2014-01-23 15:56:49 +0100

    Started to do the TreeView initialization with StyleCop rules.

--- MonoDevelop.StyleCop/OptionPanels/RulesOptionsPanel.cs
+++ MonoDevelop.StyleCop/OptionPanels/RulesOptionsPanel.cs
@@ -21,12 +21,43 @@
 namespace MonoDevelop.StyleCop
 {
   using System;
+  using MonoDevelop.Core;
+  using global::StyleCop;
 
   /// <summary>
   /// StyleCop rules options panel.
   /// </summary>
   internal partial class RulesOptionsPanel : ProjectOptionsPanelBase
   {
+
+    #region Constants
+
+    /// <summary>
+    /// The name of an analyzer node.
+    /// </summary>
+    private const string AnalyzerNode = "AnalyzerNode";
+
+    /// <summary>
+    /// The name of a parser node.
+    /// </summary>
+    private const string ParserNode = "ParserNode";
+
+    /// <summary>
+    /// The name of a rule group node.
+    /// </summary>
+    private const string RuleGroupNode = "RuleGroupNode";
+
+    /// <summary>
+    /// The name of a rule node.
+    /// </summary>
+    private const string RuleNode = "RuleNode";
+
+    #endregion
+
+    #region Private Fields
+
+    #endregion Private Fields
+
     #region Constructor
 
     /// <summary>
@@ -38,5 +69,55 @@ namespace MonoDevelop.StyleCop
     }
 
     #endregion Constructor
+
+    #region Public Override Methods
+
+    /// <summary>
+    /// Initializes the options panel values just before the panel is shown to user for the first time.
+    /// </summary>
+    /// <returns>The options panel widget.</returns>
+    /// <remarks>Will only be called if the user really gets to see the options panel.</remarks>
+    public override Gtk.Widget CreatePanelWidget()
+    {
+      Gtk.CellRendererPixbuf pixBufRenderer = new Gtk.CellRendererPixbuf();
+      Gtk.CellRendererText textRenderer = new Gtk.CellRendererText();
+      Gtk.CellRendererToggle toggleRenderer = new Gtk.CellRendererToggle();
+      toggleRenderer.Activatable = true;
+
+      Gtk.TreeViewColumn rulesColumn = new Gtk.TreeViewColumn();
+      rulesColumn.Title = GettextCatalog.GetString("Enabled rules");
+      rulesColumn.PackStart(toggleRenderer, false);
+      rulesColumn.PackStart(pixBufRenderer, false);
+      rulesColumn.PackStart(textRenderer, true);
+      rulesColumn.AddAttribute(toggleRenderer, "active", 0);
+      rulesColumn.AddAttribute(pixBufRenderer, "pixbuf", 1);
+      rulesColumn.AddAttribute(textRenderer, "text", 2);
+
+      this.treeview1.AppendColumn(rulesColumn);
+
+      Gtk.TreeStore rulesStore = new Gtk.TreeStore(typeof(bool), typeof(Gdk.Pixbuf), typeof(string));
+      this.treeview1.Model = rulesStore;
+
+      if (rulesStore != null)
+      {
+        // Add each of the parsers and analyzers to the tree.
+        foreach (SourceParser parser in this.SettingsHandler.Core.Parsers)
+        {
+          Gtk.TreeIter parserIter = rulesStore.AppendValues(true, null, parser.Name);
+
+          // Iterate through each of the analyzers and add a checkbox for each.
+          foreach (SourceAnalyzer analyzer in parser.Analyzers)
+          {
+            Gtk.TreeIter analyzerIter = rulesStore.AppendValues(parserIter, false, null, analyzer.Name);
+          }
+        }
+      }
+
+      return base.CreatePanelWidget();
+    }
+
+
+    #endregion Public Override Methods
+
   }
 }
