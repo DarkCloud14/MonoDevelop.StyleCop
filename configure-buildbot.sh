17446d04836261a71905a44bc66147e094e93cf4
Alexander Jochum <darkcloud14@gmail.com>
2016-06-27 18:16:09 +0200

    Updated Copyright...

--- configure-buildbot.sh
+++ configure-buildbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#   Copyright 2014 Alexander Jochum
+#   Copyright 2014 - 2016 Alexander Jochum
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
--- configure.fsx
+++ configure.fsx
@@ -3,7 +3,7 @@
 //   APL 2.0
 // </copyright>
 // <license>
-//   Copyright 2014 Alexander Jochum
+//   Copyright 2014-2016 Alexander Jochum
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
