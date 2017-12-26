94f3e8c460772c93b4bca92e163c76da9bee90ee
Alexander Jochum <darkcloud14@gmail.com>
2017-12-26 22:33:56 +0100

    Added StyleCop options dialog and option panels.

---
+++ MonoDevelop.StyleCop/Gui/OptionPanels/CompanyInformationOptionsPanel.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CompanyInformationOptionsPanel.cs">
+//   APL 2.0
+// </copyright>
+// <license>
+//   Copyright 2017 Alexander Jochum
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </license>
+//-----------------------------------------------------------------------
+namespace MonoDevelop.StyleCop.Gui.OptionPanels
+{
+  using MonoDevelop.Components;
+  using MonoDevelop.StyleCop.Gui.OptionPanelWidgets;
+
+  /// <summary>
+  /// StyleCop company information options panel.
+  /// </summary>
+  internal class CompanyInformationOptionsPanel : StyleCopOptionsPanel
+  {
+    #region Private Fields
+
+    /// <summary>
+    /// The company information options panel widget.
+    /// </summary>
+    private CompanyInformationOptionsPanelWidget widget = new CompanyInformationOptionsPanelWidget();
+
+    #endregion Private Fields
+
+    #region Constructor
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MonoDevelop.StyleCop.Gui.OptionPanels.CompanyInformationOptionsPanel"/> class.
+    /// </summary>
+    internal CompanyInformationOptionsPanel()
+    {
+    }
+
+    #endregion Constructor
+
+    #region Public  Override Methods
+
+    /// <summary>
+    /// Applies the changes.
+    /// </summary>
+    public override void ApplyChanges()
+    {
+    }
+
+    /// <summary>
+    /// Creates the options panel widget.
+    /// </summary>
+    /// <returns>The options panel widget.</returns>
+    public override Control CreatePanelWidget()
+    {
+      return this.widget;
+    }
+
+    /// <summary>
+    /// Determines whether this instance is visible.
+    /// </summary>
+    /// <returns><c>true</c> if this instance is visible; otherwise, <c>false</c>.</returns>
+    public override bool IsVisible()
+    {
+      return true;
+    }
+
+    #endregion Public Override Methods
+  }
+}
---
+++ MonoDevelop.StyleCop/Gui/OptionPanels/StyleCopOptionsPanel.cs
@@ -0,0 +1,76 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StyleCopOptionsPanel.cs">
+//   APL 2.0
+// </copyright>
+// <license>
+//   Copyright 2017 Alexander Jochum
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </license>
+//-----------------------------------------------------------------------
+namespace MonoDevelop.StyleCop.Gui.OptionPanels
+{
+  using System;
+  using MonoDevelop.Ide.Gui.Dialogs;
+
+  /// <summary>
+  /// StyleCop options panel base class.
+  /// </summary>
+  internal abstract class StyleCopOptionsPanel : OptionsPanel
+  {
+    #region Private Fields
+
+    /// <summary>
+    /// The settings handler object.
+    /// </summary>
+    private StyleCopSettingsHandler settingsHandler;
+
+    #endregion Private Fields
+
+    #region Protected Properties
+
+    /// <summary>
+    /// Gets the settings handler object.
+    /// </summary>
+    /// <value>The settings handler object.</value>
+    protected StyleCopSettingsHandler SettingsHandler
+    {
+      get
+      {
+        return this.settingsHandler;
+      }
+    }
+
+    #endregion Protected Properties
+
+    #region Public Override Methods
+
+    /// <summary>
+    /// Initializes the OptionsPanel.
+    /// </summary>
+    /// <param name="dialog">Parent dialog.</param>
+    /// <param name="dataObject">Data object (should be the project in our case).</param>
+    public override void Initialize(OptionsDialog dialog, object dataObject)
+    {
+      base.Initialize(dialog, dataObject);
+
+      this.settingsHandler = dataObject as StyleCopSettingsHandler;
+      if (this.settingsHandler == null)
+      {
+        throw new InvalidCastException("dataObject is not of type StyleCopSettingsHandler");
+      }
+    }
+
+    #endregion Public Override Methods
+  }
+}
